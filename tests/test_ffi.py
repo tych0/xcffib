dd5b4f0b091963c197e36ea20b3ce4249e7fadc4
Tycho Andersen <tycho@tycho.ws>
2014-06-05 21:51:25 -0500

    Fix pypy by using alternate cffi minibuffer syntax

--- tests/test_ffi.py
+++ tests/test_ffi.py
@@ -3,4 +3,4 @@ from xcffib.ffi import ffi, bytes_to_cdata
 
 def test_bytes_to_cdata():
     bs = six.b('these are some bytes')
-    assert bs == bytes(ffi.buffer(bytes_to_cdata(bs), len(bs)))
+    assert bs == ffi.buffer(bytes_to_cdata(bs), len(bs))[:]
