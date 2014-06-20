33461befd29eaf33f53442515a82d081b5206a49
Tycho Andersen <tycho@tycho.ws>
2014-06-20 00:21:42 +0000

    Solve the xcffib chicken and egg problem
    See https://groups.google.com/forum/#!topic/python-cffi/NgGybV5LLMs for more
    info.

--- setup.py
+++ setup.py
@@ -5,15 +5,28 @@ import sys
 import subprocess
 
 from setuptools import setup, find_packages
+from distutils.command.build import build
 
 if not os.path.exists('./xcffib'):
     print("It looks like you need to generate the binding.")
     print("please run 'make xcffib' or 'make check'.")
     sys.exit(1)
 
+# Stolen from http://github.com/xattr/xattr, which is also MIT licensed.
+class cffi_build(build):
+    """This is a shameful hack to ensure that cffi is present when
+    we specify ext_modules. We can't do this eagerly because
+    setup_requires hasn't run yet.
+    """
+    def finalize_options(self):
+        import xcffib
+        self.distribution.ext_modules = [xcffib.ffi.verifier.get_extension()]
+        build.finalize_options(self)
+
 # version = subprocess.check_output(['git', 'describe', '--tags'])
 
-import xcffib
+
+dependencies = ['six', 'cffi>=0.8.2']
 
 setup(
     name="xcffib",
@@ -21,8 +34,12 @@ setup(
     description="A drop in replacement for xpyb, an XCB python binding",
     keywords="xcb xpyb cffi x11 x windows",
     license="MIT",
-    install_requires=['six', 'cffi>=0.8.2'],
+    url="http://github.com/tych0/xcffib",
+    author="Tycho Andersen",
+    author_email="tycho@tycho.ws",
+    install_requires=dependencies,
+    setup_requires=dependencies,
     packages=['xcffib'],
     zip_safe=False,
-    ext_modules=[xcffib.ffi.verifier.get_extension()],
+    cmdclass={'build': cffi_build},
 )
