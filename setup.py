55634a01371ff8c642cfa60de56b6c50158354ca
Tycho Andersen <tycho@tycho.ws>
2015-03-10 08:06:22 -0600

    Release v0.2.1

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ class cffi_build(build):
         self.distribution.ext_modules = [xcffib.ffi.verifier.get_extension()]
         build.finalize_options(self)
 
-version = "0.2.0"
+version = "0.2.1"
 dependencies = ['six', 'cffi>=0.8.2']
 
 setup(
