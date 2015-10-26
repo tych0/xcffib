2a845cf68cb212ccbbfe35b745912a86e59dd2b2
Tycho Andersen <tycho@tycho.ws>
2015-10-26 13:49:11 +0900

    update the tests as well
    36c2c87e8e0996fb379b09358e67719d053232b4 changed the generated code, so
    let's update the tests as well. Whoops.

--- tests/generator/type_pad.py
+++ tests/generator/type_pad.py
@@ -51,7 +51,7 @@ class ListFontsWithInfoCookie(xcffib.Cookie):
 class type_padExtension(xcffib.Extension):
     def ListFontsWithInfo(self, max_names, pattern_len, pattern, is_checked=True):
         buf = six.BytesIO()
-        buf.write(struct.pack("=xx2xHH", max_names, pattern_len))
+        buf.write(struct.pack("=xx2xxHH", max_names, pattern_len))
         buf.write(xcffib.pack_list(pattern, "c"))
         return self.send_request(50, buf, ListFontsWithInfoCookie, is_checked=is_checked)
 xcffib._add_ext(key, type_padExtension, _events, _errors)
