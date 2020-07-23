517cd3038db7c49268b528c82ab90a7b3428cf56
Tycho Andersen <tycho@tycho.ws>
2020-07-23 07:55:20 -0600

    setup.py: fix up classifiers
    We,
    
    1. don't support python 2.7 any more
    2. are probably stable/production now, since we've been using this for
       qtile for five years
    
    Signed-off-by: Tycho Andersen <tycho@tycho.ws>

--- setup.py
+++ setup.py
@@ -82,10 +82,8 @@ setup(
         'install': binding_install
     },
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
