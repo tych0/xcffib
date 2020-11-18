6ced501b0e28cfc526065215529cdc9f251e6d2c
Tycho Andersen <tycho@tycho.pizza>
2020-11-17 21:01:59 -0500

    drop support for python 3.5
    python 3.5 has EOL'd, so let's not support it any more.
    
    Signed-off-by: Tycho Andersen <tycho@tycho.pizza>

--- setup.py
+++ setup.py
@@ -79,7 +79,6 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
