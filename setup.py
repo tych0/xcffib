069cc585457b0426a7bd797adc34f6e417f91d21
Tycho Andersen <tycho@tycho.pizza>
2023-11-10 07:35:30 -0700

    support python 3.12
    ...and drop support for 3.9. Also drop all the release qualifiers,
    hopefully nobody pays attention to these.
    
    Signed-off-by: Tycho Andersen <tycho@tycho.pizza>

--- setup.py
+++ setup.py
@@ -75,10 +75,6 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries'
